59f9f265465429ada2ba6d4b213aba1a1e1efe4e
David Khurtsidze <david.khurtsidze@wandio.com>
2021-07-21 12:39:26 +0400

    added commandcontext class

---
+++ src/CommandApi/Data/CommandContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using CommandApi.Models;
+
+namespace CommandApi.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
