efca888fc86ff3acd15b88aaae038fca1f1a32e5
David Khurtsidze <david.khurtsidze@wandio.com>
2021-07-21 13:20:23 +0400

    added migrations

---
+++ src/CommandApi/Migrations/20210721090051_AddCommandsToDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CommandApi.Migrations
+{
+    public partial class AddCommandsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HowTo = table.Column<string>(maxLength: 250, nullable: false),
+                    Platform = table.Column<string>(nullable: false),
+                    CommandLine = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
